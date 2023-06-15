cefe502c5c01533554063f8e6660582cba61ac70
qwe8851 <dgh07027@naver.com>
2023-06-15 20:54:19 +0900

    feat: install reduxjs/toolkit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+
 import reportWebVitals from './reportWebVitals';
 import { authService } from './utils/firebaseAuth';
+import store from './store/store';
+
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App authService={authService} />
+    <Provider store={store}>
+      <App authService={authService} />
+    </Provider>
   </React.StrictMode>
 );
 
