a6bc3510bb42728c97db3135957250ee09b4a9b4
qwe8851 <dgh07027@naver.com>
2023-06-15 21:00:03 +0900

    feat: 로그아웃 기능 및 사용자 인증 토큰에 따른 동적 navigation 기능 구현

--- src/components/layout/MainNavigation/Navigation.js
+++ src/components/layout/MainNavigation/Navigation.js
@@ -1,26 +1,44 @@
 import { Link, NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearAuthToken } from '../../../store/authSlice';
 
 import EventBar from './EventBar';
 
 import classes from './Navigation.module.css';
 
 const Navigation = () => {
+    const dispatch = useDispatch();
+    const token = useSelector(state => state.auth.token);
+
+    const logoutHandler = () => {
+        dispatch(clearAuthToken());
+    };
+
+    const navigationUnit = (
+        token ? <Link onClick={logoutHandler}>LogOut</Link>
+            : <>
+                <NavLink
+                    to='/auth/signin'
+                    className={({ isActive }) => (isActive ? classes.action : undefined)}
+                >
+                    SignIn
+                </NavLink>
+                <NavLink
+                    to='/auth/signup'
+                    className={({ isActive }) => (isActive ? classes.action : undefined)}
+                >
+                    SingUp
+                </NavLink>
+            </>
+    );
+
     return (
         <>
             <EventBar className={classes.eventBar} />
             <div className={classes.navigation}>
-                <p><Link to='/'>YumSeoul</Link></p>
+                <Link to='/' className={classes.logo}>YumSeoul</Link>
                 <div>
-                    <NavLink
-                        to='/auth/signin' className={({ isActive }) =>
-                            isActive ? classes.action : undefined
-                        }>
-                        SingIn
-                    </NavLink>
-                    <NavLink
-                        to='/auth/signup' className={({ isActive }) =>
-                            isActive ? classes.action : undefined
-                        }>SingUp</NavLink>
+                    {navigationUnit}
                 </div>
             </div>
         </>
